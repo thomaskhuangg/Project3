f076314f46bb8fa2b804b859726c62390b8b8cff
Thomas Huang <thomaskhuangg@gmail.com>
2020-05-15 14:48:10 -0700

    Initial Commit

---
+++ Project3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project3
+//
+//  Created by Thomas Huang on 5/15/20.
+//  Copyright © 2020 Thomas Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
